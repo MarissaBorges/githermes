b734236a4f7118a7cc9fb1e877d65afb6bec16f9
Marissa Borges <marissaborges2006@gmail.com>
2025-07-12 12:18:27 -0300

    Otimizando o scraping

--- scraper.py
+++ scraper.py
@@ -7,18 +7,15 @@ def verificar_https(url):
   if not "https" in url:
     return "https://" + url
 
-def extrair_conteudo_e_links_do_site(url):
-    url = verificar_https(url)
-    if url:
-        with sync_playwright() as pw:
-            navegador = pw.chromium.launch(headless=False)
-            pagina = navegador.new_page()
-            pagina.goto(url)
+def extrair_conteudo_e_links_do_site(pagina, url):
+    try:
+        pagina.goto(url, wait_until="domcontentloaded", timeout=15000)
+        if url:
             conteudo = pagina.content()
 
             links = []
             try:
-                soup = BeautifulSoup(conteudo, 'html.parser')
+                soup = BeautifulSoup(conteudo, 'lxml')
                 for elemento in soup.find_all('a'):
                     if isinstance(elemento, Tag):
                         if elemento.has_attr('href'):
@@ -31,7 +28,10 @@ def extrair_conteudo_e_links_do_site(url):
             markdown = md(conteudo)
 
             dados_site = {"links": links, "conteudo": markdown}
-    return dados_site
+        return dados_site
+    except Exception as e:
+        print(f"Ocorreu um erro ao acessar a página: {e}")
+        return None
 
 def baixar_documentacao(url):
     urls_ja_vistas = set()
@@ -39,21 +39,26 @@ def baixar_documentacao(url):
 
     if url not in urls_para_acessar:
         urls_para_acessar.append(url)
+    
+    with sync_playwright() as pw:
+        navegador = pw.chromium.launch(headless=False)
+        pagina = navegador.new_page()
 
-    while urls_para_acessar:
-        url_atual = urls_para_acessar.pop(0)
+        while urls_para_acessar:
+            url_atual = urls_para_acessar.pop(0)
 
-        if url_atual in urls_ja_vistas:
-            continue
+            if url_atual in urls_ja_vistas:
+                continue
 
-        dados_site = extrair_conteudo_e_links_do_site(url_atual)
-        links = dados_site['links']
-        conteudo = dados_site['conteudo']
+            dados_site = extrair_conteudo_e_links_do_site(pagina, url_atual)
+            if dados_site:
+                links = dados_site['links']
+                conteudo = dados_site['conteudo']
 
-        for link in links:
-            if link not in urls_ja_vistas:
-                urls_para_acessar.append(link)
-        urls_ja_vistas.add(url_atual)
+                for link in links:
+                    if link not in urls_ja_vistas:
+                        urls_para_acessar.append(link)
+                urls_ja_vistas.add(url_atual)
 
     return {"urls_ja_vistas": urls_ja_vistas, "urls_para_acessar": urls_para_acessar}
 
